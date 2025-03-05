deb7e04b143fedc34b4c6ce6506115ebba5bd07a
Derek Smith <dersmith@itron.com>
2025-03-05 13:33:27 -0500

    Added Json files to store history and settings

---
+++ SqlForm/Classes/ApplicationHistory.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Migrations;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
+
+namespace SqlForm.Classes
+{
+    internal static class ApplicationHistory
+    {
+        public static List<string> selectLeft = new();
+        public static List<string> selectMiddle = new();
+        public static List<string> selectRight = new();
+
+        public static bool CheckForHistory()
+        {
+            if (File.Exists("SqlFormHistory.json"))
+            {
+                return true;
+            }
+            return false;
+        }
+        public static void LoadHistory()
+        {
+            if (File.Exists("SqlFormHistory.json"))
+            {
+                string json = File.ReadAllText("SqlFormHistory.json");
+                JsonDocument doc = JsonDocument.Parse(json);
+                var historyArray = doc.RootElement.GetProperty("History").EnumerateArray();
+                foreach (var historyItem in historyArray)
+                {
+                    var selectHistoryArray = historyItem.GetProperty("SelectHistory").EnumerateArray();
+                    var insertHistoryArray = historyItem.GetProperty("InsertHistory").EnumerateArray();
+                    var deleteHistoryArray = historyItem.GetProperty("DeleteHistory").EnumerateArray();
+                    var updateHistoryArray = historyItem.GetProperty("UpdateHistory").EnumerateArray();
+                    foreach (var selectHistoryItem in selectHistoryArray)
+                    {
+                        var selectLeftJson = selectHistoryItem.GetProperty("selectLeft").EnumerateArray();
+                        var selectMiddleJson = selectHistoryItem.GetProperty("selectMiddle").EnumerateArray();
+                        var selectRightJson = selectHistoryItem.GetProperty("selectRight").EnumerateArray();
+                        foreach (var left in selectLeftJson)
+                        {
+                            if (!selectLeft.Contains(left.ToString()))
+                            {
+                                selectLeft.Add(left.ToString());
+                            }
+                        }
+                        foreach (var middle in selectMiddleJson)
+                        {
+                            if (!selectMiddle.Contains(middle.ToString()))
+                            {
+                                selectMiddle.Add(middle.ToString());
+                            }
+                        }
+                        foreach (var right in selectRightJson)
+                        {
+                            if (!selectRight.Contains(right.ToString()))
+                            {
+                                selectRight.Add(right.ToString());
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        public static bool WriteHistory()
+        {
+            var history = new
+            {
+                History = new[]
+                {
+                    new
+                    {
+                        SelectHistory = new[]
+                        {
+                            new
+                            {
+                                selectLeft,
+                                selectMiddle,
+                                selectRight
+                            }
+                        },
+                        InsertHistory = new[]
+                        {
+                            new
+                            {
+
+                            }
+                        },
+                        DeleteHistory = new[]
+                        {
+                            new
+                            {
+
+                            }
+                        },
+                        UpdateHistory = new[]
+                        {
+                            new
+                            {
+
+                            }
+                        }
+                    }
+                }
+            };
+
+
+            string json = JsonSerializer.Serialize(history, new JsonSerializerOptions { WriteIndented = true });
+            File.WriteAllText("SqlFormHistory.json", json);
+            if (CheckForHistory()) { return true; }
+            return false;
+        }
+    }
+}
---
+++ SqlForm/Classes/ApplicationSettings.cs
@@ -0,0 +1,58 @@
+﻿
+using System.Text.Json;
+using System.Xml;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace SqlForm.Classes
+{
+    internal static class ApplicationSettings
+    {
+        public static string Username = "";
+        public static string Password = "";
+        public static int MaxSqlSelection = 1;
+
+        public static void LoadSettings()
+        {
+            if (File.Exists("SqlFormSettings.json"))
+            {
+                string json = File.ReadAllText("SqlFormSettings.json");
+                JsonDocument doc = JsonDocument.Parse(json);
+                var settingsArray = doc.RootElement.GetProperty("Settings").EnumerateArray();
+                foreach (var historyItem in settingsArray)
+                {
+                    Username = historyItem.GetProperty("Username").ToString();
+                    Password = historyItem.GetProperty("Password").ToString();
+                    MaxSqlSelection = Int32.Parse(historyItem.GetProperty("MaxSqlSelection").ToString());
+                }
+            }
+        }
+        public static bool CheckForSettings()
+        {
+            if (File.Exists("SqlFormSettings.json"))
+            {
+                return true;
+            }
+            return false;
+        }
+        public static bool WriteSettings()
+        {
+            var settings = new
+            {
+                Settings = new[]
+                {
+                    new
+                    {
+                        Username,
+                        Password,
+                        MaxSqlSelection
+                    }
+                }
+            };
+
+            string json = JsonSerializer.Serialize(settings, new JsonSerializerOptions { WriteIndented = true });
+            File.WriteAllText("SqlFormSettings.json", json);
+            if (CheckForSettings()) { return true; }
+            return false;
+        }
+    }
+}
